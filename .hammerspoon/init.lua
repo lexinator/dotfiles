4649da39c2ec09235c3bad30a3fc58783a8ea5fb
Alexius Ludeman <lex@lexinator.com>
2015-07-22 10:12:22 -0400

    only reload when lua file changes
    - before any time a file created/changed in ~/.hammerspoon and this caused
      a reload(ie vi swp file), now only chnages to lua files should reload

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -245,8 +245,17 @@ end)
 
 -- auto reload when file changes
 function reload_config(files)
-    hs.reload()
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
 end
+
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/",
                    reload_config):start()
 hs.notify.show("Hammerspoon", "", "Config loaded", "")
