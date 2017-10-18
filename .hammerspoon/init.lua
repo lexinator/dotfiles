0c89401e2f417c7f2cb8b526b5383ae7d6a45ca7
Alexius Ludeman <lex@lexinator.com>
2017-10-17 21:58:30 -0700

    hotkeys for two factor auth

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -127,7 +127,23 @@ function vpn_disconnect()
         title="Hammerspoon",
         informativeText="Disabling vpn...",
     }):send()
-    os.execute("/Users/ludeman/bin/vpn vpn --action disconnect")
+    os.execute("/Users/ludeman/bin/tvpn --disconnect")
+end
+
+function totp()
+    hs.notify.new({
+        title="Hammerspoon",
+        informativeText="totp...",
+    }):send()
+    os.execute("/Users/ludeman/bin/tufa | tr -d '\n' | pbcopy")
+end
+
+function totp_uat()
+    hs.notify.new({
+        title="Hammerspoon",
+        informativeText="totp uat...",
+    }):send()
+    os.execute("/Users/ludeman/bin/tufa --LP | sed -e 's/^ //' | tr -d '\n' | pbcopy")
 end
 
 -- borrowed from
